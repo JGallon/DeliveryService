0f1b077de3f7c1d2945fc4365d4946ccad3fb1d6
JGallon <2442830653@qq.com>
2017-07-03 15:11:32 +0800

    activity_main.xml

--- app/src/main/java/com/zucc/jjl1130/deliveryservice/MainActivity.java
+++ app/src/main/java/com/zucc/jjl1130/deliveryservice/MainActivity.java
@@ -2,12 +2,8 @@ package com.zucc.jjl1130.deliveryservice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVOSCloud;
-import com.avos.avoscloud.AVObject;
-import com.avos.avoscloud.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +14,5 @@ public class MainActivity extends AppCompatActivity {
         AVOSCloud.initialize(this, "9Bey6hIH7AAIV17V8yzyFj9f-gzGzoHsz", "4HV9Drjm63B4zAeVXwu3pHwa");
         AVOSCloud.setDebugLogEnabled(true);
 
-        AVObject testObject = new AVObject("TestObject");
-        testObject.put("words", "Hello World!");
-        testObject.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(AVException e) {
-                if (e == null) {
-                    Log.d("saved", "success!");
-                }
-            }
-        });
     }
 }
