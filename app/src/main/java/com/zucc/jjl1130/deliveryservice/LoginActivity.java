5b547f0c93736c6602b944eff987dc77e72bb660
JGallon <2442830653@qq.com>
2017-07-04 10:33:53 +0800

    activity_login.xml and activity_register.xml

--- app/src/main/java/com/zucc/jjl1130/deliveryservice/LoginActivity.java
+++ app/src/main/java/com/zucc/jjl1130/deliveryservice/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.zucc.jjl1130.deliveryservice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/zucc/jjl1130/deliveryservice/MainActivity.java
+++ app/src/main/java/com/zucc/jjl1130/deliveryservice/MainActivity.java
@@ -1,18 +1,29 @@
 package com.zucc.jjl1130.deliveryservice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.avos.avoscloud.AVOSCloud;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+//    @BindView(R.id.btn_in)
+//    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AVOSCloud.initialize(this, "9Bey6hIH7AAIV17V8yzyFj9f-gzGzoHsz", "4HV9Drjm63B4zAeVXwu3pHwa");
-        AVOSCloud.setDebugLogEnabled(true);
-
+//        AVOSCloud.initialize(this, "9Bey6hIH7AAIV17V8yzyFj9f-gzGzoHsz", "4HV9Drjm63B4zAeVXwu3pHwa");
+//        AVOSCloud.setDebugLogEnabled(true);
+        Button btn = (Button) findViewById(R.id.btn_in);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
